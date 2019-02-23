a325732b5ff8be076f4d78111dd5fc3534213ecc
Nosa Obaseki <fredobaseki@gmail.com>
2019-02-23 03:31:58 +0100

    configured webpack to remove all console.logs, sourcemaps and strip out the local for momentjs

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,24 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const webpack = require('webpack')
+const isProd = process.env.NODE_ENV === "production"
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  configureWebpack: {
+    optimization: {
+      minimizer: isProd ? [
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            compress: {
+              warnings: false,
+              drop_console: true
+            },
+            sourceMap: false
+          }
+        })
+      ] : []
+    },
+    plugins: [
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+    ]
+  }
 }
